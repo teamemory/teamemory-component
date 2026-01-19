474d1b7287b6530505e08a232e36662a52f1b595
teamemory <zhangjun@rockontrol.com>
2026-01-19 10:51:57 +0800

    feat:init

---
+++ examples/App.jsx
@@ -0,0 +1,10 @@
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+  props: {},
+  setup: (props, {}) => {
+    return () => {
+      return <div>12121</div>;
+    };
+  },
+});
--- examples/main.ts
+++ examples/main.ts
@@ -1,5 +1,5 @@
 import { createApp } from 'vue';
-import App from './App.vue';
+import App from './App.jsx';
 import ElementPlus from 'element-plus';
 import 'element-plus/dist/index.css';
 

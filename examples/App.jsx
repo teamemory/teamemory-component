d6ec12c04168ff6e10092282822f5fe9c967bdf9
teamemory <zhangjun@rockontrol.com>
2026-01-27 14:21:04 +0800

    feat

---
+++ examples/App.jsx
@@ -0,0 +1,11 @@
+import { defineComponent, reactive, ref } from 'vue';
+
+export default defineComponent({
+  props: {},
+  setup: (props, {}) => {
+
+    return () => {
+      return <div>测试的12121</div>;
+    };
+   },
+});

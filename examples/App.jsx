c106f8c38780f6ca93e3feb5f324fd87c10eb8da
teamemory <zhangjun@rockontrol.com>
2026-01-27 14:29:44 +0800

    feat

--- examples/App.jsx
+++ examples/App.jsx
@@ -1,11 +1,15 @@
-import { defineComponent, reactive, ref } from 'vue';
+import { defineComponent, reactive, ref } from "vue";
+import { TmButton } from "../src";
 
 export default defineComponent({
   props: {},
   setup: (props, {}) => {
-
     return () => {
-      return <div>测试的12121</div>;
+      return (
+        <div>
+          测试的12121 <TmButton size={'middle'}>测试的</TmButton>
+        </div>
+      );
     };
-   },
+  },
 });
